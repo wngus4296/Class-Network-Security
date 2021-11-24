9a0e2561313383d57940b91a31426178356ed55c
JuHyeon Byun <58043306+wngus4296@users.noreply.github.com>
2021-11-24 15:48:32 +0900

    [FEAT] daytime 실습
    1. windows.h와 winsock2.h의 충돌이 있어서 강의자료에 있는 그대로 사용하면 에러 발생 (Windows OS의 경우)
        winSock2.h 헤더가 먼저 include 되어야함
    
    2. 헤더파일은 필수적인 것만 포함
        err_sys 등의 에러 처리 함수는 단순히 화면 출력을 위해 printf로 수정
    
    3. server 소스파일 내 ctime() 함수가 보안적 문제로 에러를 띄우기 때문에 상단에 에러 무시 매크로 추가
    
    -----
    client와 server는 독립적인 프로젝트여야 함.
    client 프로젝트에 daytimetcpcli.cpp 와 unp.h 추가
    server 프로젝트에 daytimetcpsvr.cpp 와 unp.h 추가
    각기 컴파일 이후, server를 먼저 실행 --> 방화벽 경고 문구 --> 예외 추가
    cmd 콘솔창을 열어 "daytimetcpcli.exe 127.0.0.1" 실행 
    시간 날짜가 제대로 출력되는지 확인

---
+++ daytime/daytimetcpcli.cpp
@@ -0,0 +1,40 @@
+﻿#include "unp.h"
+
+int main(int argc, char* argv[])
+{
+    int sockfd, n;    // sockfd 소켓 디스크립터 (번호표), n은 시간 정보의 크기 (바이트 단위)
+    char recvline[MAXLINE + 1]; // 서버로부터 받을 시간 정보를 저장할 버퍼
+    struct sockaddr_in servaddr; // 서버의 주소를 저장한 인터넷 소켓 주소 구조체
+    // 시간 서버의 IP 주소가 인자로 주어졌는지를 체크
+    if (argc != 2)
+        printf("usage: %s <IPaddress>", argv[0]);
+#ifdef _WIN32
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);//Windows Sockets API Version 2.2
+#endif
+    // TCP 소켓을 생성해 socket descriptor를 받아와서 sockfd에 저장
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        printf("socket error");
+    // 서버 주소에 대한 구조체 servaddr에 주소를 설정함
+    bzero(&servaddr, sizeof(servaddr)); // 0으로 채움
+    servaddr.sin_family = AF_INET; // Internet Protocol Suite를 사용
+    servaddr.sin_port = htons(13); /* daytime server port number */
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+        printf("inet_pton error for %s", argv[1]); // (argv[1] 문자열)에 저장된 dotted 
+                                         // decimal 형식의 서버 주소를 32비트 이진수로 변환
+        // servaddr에 저장된 주소로 연결을 시도함
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0)
+        printf("connect error");
+    // 연결된 소켓을 통해 서버가 보낸 바이트 열 (즉, 읽을 문장)이 있으면
+    while ((n = recv(sockfd, recvline, MAXLINE, 0)) > 0) { // 이를 recvline에 저장함
+        recvline[n] = 0; // buffer overflow를 막기 위해 마지막 글자는 널 캐릭터로 바꿈
+        if (fputs(recvline, stdout) == EOF) // stdout에 recvline을 밀어넣음
+            printf("fputs error"); // 코드 구조 상 fputs의 결과가 EOF가 나올 수 없음
+    }
+    if (n < 0) // 정상적이었다면 n == 0이어야 하며, n < 0이면 while 루프에 문제가 있었음
+        printf("read error");
+#ifdef _WIN32
+    WSACleanup(); // Winsock 사용 후 정리
+#endif
+    exit(0);
+}
---
+++ daytime/daytimetcpsrv.cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "unp.h"
+#include <time.h>
+
+int main(int argc, char* argv[]) {
+    int listenfd, connfd; // client를 기다리는 listenfd와, 연결 수립 후 사용하는 connfd
+    char buff[MAXLINE];   // client에 보낼 내용을 저장하는 buffer
+    struct sockaddr_in servaddr; // server가 기다리려는 client의 주소 범위를 저장함
+    time_t ticks;
+#ifdef _WIN32
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+#endif
+    // TCP 소켓을 생성해 socket descriptor를 받아와서 sockfd에 저장
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        printf("socket error");
+    // 서버 주소에 대한 구조체 servaddr에 주소를 설정함
+    bzero(&servaddr, sizeof(servaddr));           // 0으로 채움
+    servaddr.sin_family = AF_INET;                // Internet Protocol Suite를 사용
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // 0으로 채우는 것과 동일함
+    servaddr.sin_port = htons(13);                /* daytime server port number */
+    // servaddr에 저장된 주소를 소켓에 bind함
+    if (bind(listenfd, (SA*)&servaddr, sizeof(servaddr)) < 0)
+        printf("bind error");
+    // 공간을 줄이기 위해 socket sockaddr는 SA로 치환함. connect는 SA* 타입을 받아야 하는데   
+    // &servaddr는 struct sockaddr_in* 타입으로 다른 상황이므로, 타입 캐스팅이 필요함
+        // bind한 소켓을 통해 client의 접속을 대기 (listen)함
+    int backlog = LISTENQ; // listen 함수가 리턴될 때까지 받을 수 있는 클라이언트의 최대 수
+    if (listen(listenfd, backlog) < 0)
+        printf("listen error");
+    for (;;) {
+    again:
+        if ((connfd = accept(listenfd, (SA*)NULL, NULL)) < 0)
+            if (errno == EPROTO || errno == ECONNABORTED) /* SW 문제로 연결 중단 */
+                goto again;
+            else
+                printf("accept error");
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        // 수립된 연결에 대한 socket descriptor인 connfd에 저장된 시간 문자열을 기록
+
+        if (send(connfd, buff, strlen(buff), 0) != strlen(buff))
+            printf("send error");
+        if (closesocket(connfd) == SOCKET_ERROR)
+            printf("close error");
+
+    }
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    exit(0);
+}
